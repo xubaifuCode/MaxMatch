3b2e85869ebfef0b47c2d2be29f461c68bb0c3ca
xubaifuCode <1660866966@qq.com>
2016-09-25 15:22:49 +0800

    Change

--- js/js.js
+++ js/js.js
@@ -10,7 +10,25 @@ function generationPeople(gender, v) {
 	var obj = $(gender + "_area");
 	var str = ""
 	for (var i = 1; i <= v; i++) {
-		str += "<span class=\"" + gender + "\" id=\"" + gender + "_" + i + "\">" + i + "</span>"
+		str += "<div class=\"" + gender + "\" id=\"" + gender + "_" + i + "\">" + i + "</div>"
 	}
 	obj.innerHTML += str;
+}
+
+function startToMatch() {
+	moveTo("boy_1", 300);
+	moveTo("girl_1", -300);
+	setTimeout(cancelDisplay("boy_1"), 3000);
+	//setTimeout(cancelDisplay("girl_1"), 3000);
+}
+
+function cancelDisplay(elemId) {
+	$(elemId).remove();
+}
+
+function moveTo(elemId, toX) {
+	//$(elemId).style.marginLeft = toX + "px";
+	//$(elemId).style.display = "none";
+	$(elemId).setAttribute("class", toX > 0 ? "cancel_boy" : "cancel_girl");
+
 }
