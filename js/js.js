33f1515849fd06152a516b4bb555302970805d1a
xubaifuCode <1660866966@qq.com>
2016-09-25 13:03:52 +0800

    自动生成元素

--- js/js.js
+++ js/js.js
@@ -1,3 +1,16 @@
 function $(elemId) {
 	return document.getElementById(elemId);
 }
+
+function generationPeople(gender, v) {
+	if (isNaN(v)) {
+		alert("Error caused by number of " + gender);
+		return;
+	}
+	var obj = $(gender + "_area");
+	var str = ""
+	for (var i = 1; i <= v; i++) {
+		str += "<span class=\"" + gender + "\" id=\"" + gender + "_" + i + "\">" + i + "</span>"
+	}
+	obj.innerHTML += str;
+}
